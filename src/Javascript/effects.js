ebfa3d85b65abccfa72bdbad1eedd87b94b7095e
Tuncarrot <adastunkiewicz@gmail.com>
2020-03-21 15:11:57 -0400

    :bug: fix react syntax

--- src/Javascript/effects.js
+++ src/Javascript/effects.js
@@ -1,10 +1,10 @@
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = () => {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbarHide").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbarHide").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 }
