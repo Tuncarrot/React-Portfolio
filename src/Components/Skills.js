9c3e2b59660a5095c92ddaeffe9887f3cad8d544
Tuncarrot <adastunkiewicz@gmail.com>
2020-03-21 23:43:08 -0400

    :art: add array argument to function

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 const Skills = (props) => {
+    var skillsList = props.list;
+    var formattedList = [];
+
+    for (var i=0; i<skillsList.length;i++)
+    {
+        formattedList.push(<span className="badge badge-pill badge-primary skills-pills">{skillsList[i]}</span>)
+    }
+
     return (
     <div>
         <div className="row justify-content-center title-styling">
             <h5>{props.title}</h5>
         </div>
         <div className="row justify-content-center">
-            <span className="badge badge-pill badge-primary skills-pills">C</span>
+            {formattedList}
         </div>
     </div>
     );
